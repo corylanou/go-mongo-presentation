2e3c1de2b321924b9e23792f7cd233e4c2cd95f7
Cory LaNou <cory@lanou.com>
2015-01-10 10:29:41 -0700

    mongo session completed

--- mongo/01_session/main.go
+++ mongo/01_session/main.go
@@ -1,12 +1,27 @@
+//START IMPORT OMIT
 package main
 
 import (
 	"fmt"
+	"os"
 
 	"gopkg.in/mgo.v2"
 )
 
+//END IMPORT OMIT
+
+//START MAIN OMIT
 func main() {
+	// END MAIN OMIT
+
+	// START ENV OMIT
+	var (
+		mongoUrl        = os.Getenv("mongo_url")
+		mongoUser       = os.Getenv("mongo_user")
+		mongoPassword   = os.Getenv("mongo_password")
+		mongoCollection = os.Getenv("mongo_collection")
+	)
+	// END ENV OMIT
 
 	var (
 		mongoSession *mgo.Session
@@ -14,14 +29,35 @@ func main() {
 		collection   *mgo.Collection
 		err          error
 	)
+	//END IMPORT OMIT
 
-	// START OMIT
-	if mongoSession, err = mgo.Dial("localhost"); err != nil {
+	// START DIAL OMIT
+	/*
+		Format of string for dialing is as follows:
+
+			mongodb://myuser:mypass@localhost:40001,otherhost:40001/mydb
+	*/
+	addr := fmt.Sprintf(
+		"mongodb://%s:%s@%s",
+		mongoUser,
+		mongoPassword,
+		mongoUrl,
+	)
+
+	if mongoSession, err = mgo.Dial(addr); err != nil {
 		panic(err)
 	}
-	database = mongoSession.DB("mgo_examples")
-	collection = database.C("to_dos")
-	// END OMIT
+	defer mongoSession.Close()
+	// END DIAL OMIT
 
-	fmt.Printf("Collection: %# v", pretty.Formatter(collection))
+	// START FINAL OMIT
+	// This will get the "default" database that the connection string specified
+	database = mongoSession.DB("")
+
+	// Get our collection
+	collection = database.C(mongoCollection)
+
+	// For debuging, print out the collection we found
+	fmt.Printf("Collection: %+v", collection)
+	// END FINAL OMIT
 }
